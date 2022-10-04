6bd770033b15c9f84f8041c7f74f0eae24b77df3
patricklaireiter <95612263+patricklaireiter@users.noreply.github.com>
2022-10-04 11:17:28 +0200

    implemented basic add functionality for todo list

---
+++ src/components/TodoList.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref } from "vue";
+const list = ref([])
+const newItem = ref("");
+ function addItem(){
+list.value.push(newItem.value);
+newItem.value = "";  
+
+}
+function deleteItem(){
+newItem.value = "";  
+}
+
+</script>
+
+<template>
+  <h1>TodoList</h1>
+    <input type="text" v-model="newItem" placeholder="neue Aufgabe" />
+<button @click="addItem">Add</button>
+<ul>
+    <li v-for="item in list" :key="item">{{item}}</li></ul>
+    <button @click="deleteItem">Delete</button>
+</template>
